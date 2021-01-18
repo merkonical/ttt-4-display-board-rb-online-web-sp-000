1638cf5b2e4899152946207d4bc6843447f58d3c
merkonical <merkonical@gmail.com>
2021-01-17 22:37:45 -0800

    commit tree 19262337045a798163b3b29fc28b28c47b907d3c

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,20 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+#board = [" "," "," "," "," "," "," "," "," "]
+#display_board(board)
+#board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+    
+
+    rows = []
+    rows[0] = " #{board[0]} | #{board[1]} | #{board[2]} "
+    rows[1] = "-----------"
+    rows[2] = " #{board[3]} | #{board[4]} | #{board[5]} "
+    rows[3] = "-----------"
+    rows[4] = " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts rows
+end
+
+
+
